d7b1962553ce1d34d7c7ba6446af93cce691c3a6
牛海青 <haiqing1@staff.sina.com.cn>
2014-10-28 17:02:54 +0800

    Add admin port.

--- src/rash_socks.erl
+++ src/rash_socks.erl
@@ -2,20 +2,32 @@
 
 -export([start/0]).
 
--define(DEFAULT_CONFIG, [{port, 10800}, {addr, "0.0.0.0"}, {connect_timeout, 2000}, {shortlist_size, 256}]).
+-import(config, [config/2]).
+
+-define(DEFAULT_CONFIG, [{port, 10800}, {addr, "0.0.0.0"}, {admin_port, 8972}, {connect_timeout, 2000}, {shortlist_size, 256}]).
 
 start() ->
 	start(?DEFAULT_CONFIG).
 start(Config) ->
 	queuectl:create(Config),
-	{port, Port} = lists:keyfind(port, 1, Config),
-	{addr, Addr} = lists:keyfind(addr, 1, Config),
-	Pid = simple_tcp_server:create({Addr, Port, []}, {socks4, socks4_callback, [Config]}),
-	loop(Pid).
+	simple_tcp_server:create({config(addr, Config), config(port, Config), []}, {socks4, socks4_callback, [Config]}),
+	{ok, Socket} = gen_tcp:listen(config(admin_port, Config), [inet, {active, true}, {packet, http}]),
+	admin_start(Socket, Config).
 
-loop(Pid) ->
-	receive
+admin_start(Socket, Config) ->
+	admin_loop(Socket, Config).
+
+admin_loop(Socket, Config) ->
+	case gen_tcp:accept(Socket) of
+		{ok, Client} ->
+			Req = get_request(Client);
 		_ ->
-			loop(Pid)
+			admin_loop(Socket, Config)
+	end.
+
+get_request(Socket) -> get_request(Socket, dict:new()).
+get_request(Socket, Req) ->
+	receive
+		{http_request } -> ok
 	end.
 
